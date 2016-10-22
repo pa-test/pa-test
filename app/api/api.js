5ef00b1b801a50ab04dea19eede275c2802f7165
Stuart Phillips <stuartphillips25@gmail.com>
2016-10-22 18:55:41 +0100

    Finish subscription implementation

--- app/api/api.js
+++ app/api/api.js
@@ -37,20 +37,44 @@ app.post("/upload", function(req, res) {
 
 app.post("/subscribe", function(req, res) {
   if (req.body) {
-    var user = new User(req.body);
-    var invalidFormat = user.validateSync();
-    if (!invalidFormat) {
-      user.save(function(err) {
-        if (err) {
-          console.log(err);
-          res.sendStatus(400);
-        } else {
-          res.sendStatus(200);
-        }
-      });
-    } else {
-      res.sendStatus(400);
-    }
+  	var user = new User(req.body);
+		var invalidFormat = user.validateSync();
+		if (invalidFormat) {
+			res.sendStatus(400);
+			return;
+		}
+
+    User.findOne({'email': req.body.email}, 'categories', function(err, result) {
+    	if (!result) {  // create a new entry if this user doesn't exist
+				user.save(function(err) {
+	        if (err) {
+	          console.log(err);
+	          res.sendStatus(500);  // internal server error
+	        } else {
+	          res.sendStatus(200);
+	        }
+	      });
+    	} else {  // update subscription for existing user
+    		var newCategories = req.body.categories.filter(function(item) {
+    			return result.categories.indexOf(item) == -1;
+    		});
+
+    		if (newCategories.length > 0) {
+    			var updatedList = result.categories.concat(newCategories);
+	    		User.update({email: req.body.email}, {categories: updatedList}, function(err, raw) {
+					  if (err) {
+					  	console.log(err);
+					  	res.sendStatus(500);
+					  } else {
+					  	res.sendStatus(200);
+					  }
+					});
+    		} else {
+    			res.sendStatus(200);  // do nothing if the request categories are identical
+    		}
+
+    	}
+    });
   } else {
     res.sendStatus(400);
   }
@@ -61,7 +85,14 @@ app.post("/unsubscribe", function(req, res) {
 });
 
 app.post("/reset", function(req, res) {
-  res.sendStatus(501);
+  User.remove({}, function(err) {
+  	if (err) {
+  		console.log(err);
+  		res.sendStatus(500);
+  	} else {
+  		res.sendStatus(200);
+  	}
+  });
 });
 
 // Initialize the app
--- test/index.js
+++ test/index.js
@@ -4,6 +4,10 @@
 var request = require('request');
 var expect = require("chai").expect;
 var fs = require('fs');
+var mongoose = require('mongoose');
+
+var Article = require("../app/schemas/article.js").Article;
+var User = require("../app/schemas/user.js").User;
 
 var newsApi = require("../app/api/api.js");
 var baseUrl = "http://localhost:" + newsApi.server.address().port;
@@ -94,7 +98,11 @@ describe("News API", function() {
   	it("removes all user subscriptions", function(done) {
   		request.post({url: baseUrl + "/reset"}, function(err, res, body) {
   			expect(res.statusCode).to.equal(200);
-  			done();
+
+        User.where({}).count(function(err, count) {
+          expect(count).to.equal(0);
+          done();
+        });
   		});
   	});
 
@@ -103,24 +111,64 @@ describe("News API", function() {
   describe("user subscription endpoint", function() {
 
     it("subscribes a user to provided categories", function(done) {
-    	request.post({url: baseUrl + "/subscribe", body: {email: "johnsmith@gmail.com", categories: ["sports", "world"]}, json: true}, function(err, res, body) {
+      var subscription = {email: "johnsmith@gmail.com", categories: ["sports", "world"]};
+    	request.post({url: baseUrl + "/subscribe", body: subscription, json: true}, function(err, res, body) {
         expect(res.statusCode).to.equal(200);
-        request.post({url: baseUrl + "/reset"});
-        done();
+
+        User.where(subscription).count(function(err, count) {
+          expect(count).to.equal(1);
+
+          request.post({url: baseUrl + "/reset"}, function(err, res, body) {
+            expect(res.statusCode).to.equal(200);
+            done();
+          });
+        });
       });
     });
 
-    it("ignores valid provided categories that the user is already subscribed to", function(done) {
-    	request.post({url: baseUrl + "/subscribe", body: {email: "johnsmith@gmail.com", categories: ["sports", "world"]}, json: true}, function(){});
-    	request.post({url: baseUrl + "/subscribe", body: {email: "johnsmith@gmail.com", categories: ["sports", "world"]}, json: true}, function(err, res, body) {
+    it("ignores duplicate subscriptions", function(done) {
+      var subscription = {email: "johnsmith@gmail.com", categories: ["sports", "world"]};
+      request.post({url: baseUrl + "/subscribe", body: subscription, json: true}, function(err, res, body) {
         expect(res.statusCode).to.equal(200);
-        request.post({url: baseUrl + "/reset"});
-        done();
+
+        request.post({url: baseUrl + "/subscribe", body: subscription, json: true}, function(err, res, body) {
+          expect(res.statusCode).to.equal(200);
+
+          User.where(subscription).count(function(err, count) {
+            expect(count).to.equal(1);
+            request.post({url: baseUrl + "/reset"}, function(err, res, body) {
+              expect(res.statusCode).to.equal(200);
+              done();
+            });
+          });
+        });
+      });
+    });
+
+    it("updates a subscription with new provided categories", function(done) {
+      var subscription = {email: "johnsmith@gmail.com", categories: ["sports", "world"]};
+      request.post({url: baseUrl + "/subscribe", body: subscription, json: true}, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+
+        subscription.categories = ["sports", "world", "technology"];
+        request.post({url: baseUrl + "/subscribe", body: subscription, json: true}, function(err, res, body) {
+          expect(res.statusCode).to.equal(200);
+
+          User.find({email: subscription.email}, 'categories', function(err, result) {
+            expect(result.length).to.equal(1);  // check that the email hasn't been duplicated
+            expect(result[0].categories == subscription.categories);  // check that the categories updated correctly
+
+            request.post({url: baseUrl + "/reset"}, function(err, res, body) {
+              expect(res.statusCode).to.equal(200);
+              done();
+            });
+          });
+        });
       });
     });
 
     it("fails without provided categories", function(done) {
-    	request.post({url: baseUrl + "/subscribe", body: {email: "johnsmith@gmail.com"}}, function(err, res, body) {
+    	request.post({url: baseUrl + "/subscribe", body: {email: "johnsmith@gmail.com"}, json: true}, function(err, res, body) {
         expect(res.statusCode).to.equal(400);
         done();
       });
